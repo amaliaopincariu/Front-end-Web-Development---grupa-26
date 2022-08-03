43f0b96aa18ac69275def556028ff31e8227662b
Razvan Timis <razvan.timis@8x8.com>
2022-08-03 21:03:57 +0300

    Add dayjs

--- Week12 - Intro tools - react/exemples/spa/my-first-app/pages/getHomePage.js
+++ Week12 - Intro tools - react/exemples/spa/my-first-app/pages/getHomePage.js
@@ -1,14 +1,25 @@
 import './home.css'
+import dayjs from 'dayjs';
 
 const getHomePage = () => {
+  const today = dayjs().format('DD/MM/YYYY');
+  console.log(today);
   return `
       <h2 class="home-title">Home page</h2>
-
+      ${today}
     <p>lorem ipsum dolor sit amet, consectetur adipiscing elit.
     </p>
     <p>lorem ipsum dolor sit amet, consectetur adipiscing elit.
     </p>
   `
+  
 }
 
 export default getHomePage;
+
+/*
+import dayjs from 'dayjs'
+
+console.log(dayjs().format('DD/MM/YYYY'))
+
+*/
