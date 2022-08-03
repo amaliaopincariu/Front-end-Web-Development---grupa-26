8ef785d9352808af1273070dc9dfb1785a64adfc
Razvan Timis <razvan.timis@8x8.com>
2022-08-03 20:10:46 +0300

    Add switch pages
    Co-authored-by: Andrei Bulgarasu <BulgarasuA@users.noreply.github.com>
    Co-authored-by: GDaniel96 <GDaniel96@users.noreply.github.com>
    Co-authored-by: PikaDaniel <PikaDaniel@users.noreply.github.com>
    Co-authored-by: Andreea Adriana Miclaus <andreeamiclaus@users.noreply.github.com>
    Co-authored-by: Nicolae Culda <Nicu91@users.noreply.github.com>
    Co-authored-by: LauraLucian <LauraLucian@users.noreply.github.com>
    Co-authored-by: Busecan Rares <dino4030@users.noreply.github.com>
    Co-authored-by: alexteo <alexteo@users.noreply.github.com>
    Co-authored-by: Mihai Dragan <teraenas@users.noreply.github.com>
    Co-authored-by: MafteiMugurel <MafteiMugurel@users.noreply.github.com>
    Co-authored-by: nicoara34 <nicoara34@users.noreply.github.com>
    Co-authored-by: DavidescuAdriana1978 <DavidescuAdriana1978@users.noreply.github.com>

--- Week12 - Intro tools - react/exemples/spa/my-first-app/main.js
+++ Week12 - Intro tools - react/exemples/spa/my-first-app/main.js
@@ -1,16 +1,48 @@
 import './style.css'
 
-const app = document.querySelector('#app');
-app.innerHTML = `
-<header>
-  <h1>My Personal Page</h1>
-  <nav>
-    <ul>
-      <li><a href="#">Home</a></li>
-      <li><a href="#">About</a></li>
-    </ul>
-  </nav>
-</header>
+const appHtmlDiv = document.querySelector('#app');
+appHtmlDiv.innerHTML = `
+  <header>
+    <h1>My Personal Page</h1>
+    <nav>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+      </ul>
+    </nav>
+  </header>
+  <main id="app-main-content">
+  </main>
+`;
+
+const homeButton = document.querySelector('header nav ul li:first-child a');
+const aboutButton = document.querySelector('header nav ul li:nth-child(2) a');
+const mainContent = appHtmlDiv.querySelector('#app-main-content');
+
+const homeContent = `
+<h2>Home page</h2>
+
+<p>lorem ipsum dolor sit amet, consectetur adipiscing elit.
+</p>
+<p>lorem ipsum dolor sit amet, consectetur adipiscing elit.
+</p>
 
 `
+
+mainContent.innerHTML = homeContent;
+
+homeButton.addEventListener('click', () => {
+  mainContent.innerHTML = homeContent;
+})
+aboutButton.addEventListener('click', () => {
+  mainContent.innerHTML = `
+  <h2>About page</h2>
+   <p>My phone is 056756756</p>
+  <p>lorem ipsum dolor sit amet, consectetur adipiscing elit.
+  </p>
+  <p>lorem ipsum dolor sit amet, consectetur adipiscing elit.
+  </p>
+  `;
+})
+
 
