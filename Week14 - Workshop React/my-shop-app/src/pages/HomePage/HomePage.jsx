edd961653df33257fab71b9087efc6ffd25860aa
Razvan Timis <razvan.timis@8x8.com>
2022-08-17 20:14:06 +0300

    Add fetch products Co-authored-by: Andrei Bulgarasu <BulgarasuA@users.noreply.github.com> Co-authored-by: GDaniel96 <GDaniel96@users.noreply.github.com> Co-authored-by: Mihai Dragan <teraenas@users.noreply.github.com> Co-authored-by: Nicolae Culda <Nicu91@users.noreply.github.com> Co-authored-by: PikaDaniel <PikaDaniel@users.noreply.github.com> Co-authored-by: Andreea Adriana Miclaus <andreeamiclaus@users.noreply.github.com> Co-authored-by: DavidescuAdriana1978 <DavidescuAdriana1978@users.noreply.github.com>

--- Week14 - Workshop React/my-shop-app/src/pages/HomePage/HomePage.jsx
+++ Week14 - Workshop React/my-shop-app/src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,30 @@
+import { useEffect, useState } from 'react'
 import './HomePage.css'
-
+import productList21 from './produse.json'; 
+// [1,2,3].map(value => value*2) => [2, 4, 6]
 const HomePage = () => {
+  const [productList, setProductList] = useState([])
+  useEffect(() => {
+    // fetch('https://fakestoreapi.com/products')
+    //   .then(res => res.json())
+    //   .then(json => {
+    //     setProductList(json)
+    //   })
+  }, []) // doar odata se executa
+
   return (
     <>
-      <h2 className="home-title">Home page</h2>
-       lista de produse
+      <div className="product-list">
+        {productList21.map((productObject) => {
+          return (
+            <div className='product'>
+              <h1>{productObject.title}</h1>
+              <img src={productObject.image}></img >
+              <p>{productObject.price} RON</p>
+            </div>
+          )
+        })}
+      </div>
     </>
   )
 
